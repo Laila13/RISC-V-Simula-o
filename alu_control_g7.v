c502a354c2fe279bf5ae6f654a02c6f086ecd8f1
Laila13 <77805862+Laila13@users.noreply.github.com>
2025-08-15 20:53:22 -0300

    alu_control_g7.v

--- alu_control_g7.v
+++ alu_control_g7.v
@@ -1,20 +1,24 @@
-// alu_control_g7.v
 `include "riscv_defines_g7.v"
+
 module alu_control_g7 (
-    input  wire [1:0] ALUOp,    input wire [6:0] funct7,
-    input  wire [2:0] funct3,   output reg [3:0] ALUControl
+    input  wire [1:0] ALUOp,
+    input  wire [6:0] funct7,
+    input  wire [2:0] funct3,
+    output reg  [3:0] ALUControl
 );
     always @(*) begin
         case (ALUOp)
-            2'b00: ALUControl = `ALU_ADD;
-            2'b01: ALUControl = `ALU_SUB;
-            2'b10: case (funct3)
+            2'b00: ALUControl = `ALU_ADD; // Para ADDI, LB, SB
+            2'b01: ALUControl = `ALU_SUB; // Para BEQ
+            2'b10: begin // Para instruções Tipo-R
+                case (funct3)
                     3'b000: ALUControl = `ALU_ADD;
                     3'b110: ALUControl = `ALU_OR;
                     3'b111: ALUControl = `ALU_AND;
                     3'b101: ALUControl = `ALU_SRL;
                     default: ALUControl = 4'bxxxx;
-                   endcase
+                endcase
+            end
             default: ALUControl = 4'bxxxx;
         endcase
     end
