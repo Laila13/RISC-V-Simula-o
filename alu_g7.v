7acdef083ad346e1ca89e595fd2bdc42bcf7fdb5
Laila13 <77805862+Laila13@users.noreply.github.com>
2025-08-15 21:03:20 -0300

    alu_g7.v

--- alu_g7.v
+++ alu_g7.v
@@ -1,8 +1,7 @@
-// alu_g7.v
 `include "riscv_defines_g7.v"
+
 module alu_g7 (
-    input  wire [31:0] a,
-    input  wire [31:0] b,
+    input  wire [31:0] a, b,
     input  wire [3:0]  ALUControl,
     output reg  [31:0] result,
     output wire        zero
@@ -14,7 +13,7 @@ module alu_g7 (
             `ALU_AND: result = a & b;
             `ALU_OR:  result = a | b;
             `ALU_SRL: result = a >> b[4:0];
-            default:  result = 32'hxxxxxxxx;
+            default: result = 32'hxxxxxxxx;
         endcase
     end
     assign zero = (result == 32'b0);
