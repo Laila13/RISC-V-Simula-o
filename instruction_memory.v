b69979c33a4da49fb47be693cbba8a0e0f746a0e
Laila13 <77805862+Laila13@users.noreply.github.com>
2025-08-15 21:05:13 -0300

    instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,9 +1,5 @@
-// instruction_memory.v
-module instruction_memory (
-    input  wire [31:0] address,
-    output wire [31:0] instruction
-);
-    reg [31:0] mem [1023:0];
-    initial $readmemh("meu_programa_g7.hex", mem);
-    assign instruction = mem[address[31:2]];
+module instruction_memory (input wire [31:0] address, output wire [31:0] instruction);
+    reg [31:0] memory [255:0];
+    initial $readmemh("meu_programa_g7.hex", memory);
+    assign instruction = memory[address >> 2];
 endmodule
