ad92119dec2f71bae268d694bc65c341fbcd5c5e
Laila13 <77805862+Laila13@users.noreply.github.com>
2025-08-15 21:09:02 -0300

    testbench_g7.v

--- testbench_g7.v
+++ testbench_g7.v
@@ -1,19 +1,13 @@
-// testbench_g7.v
 `timescale 1ns/1ps
 module testbench_g7;
-    reg clk;
-    reg rst;
+    reg clk, rst;
     riscv_processor_g7 dut (.clk(clk), .rst(rst));
+    initial begin clk=0; forever #5 clk=~clk; end
     initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
-    end
-    initial begin
-        $dumpfile("waveform.vcd");
-        $dumpvars(0, dut);
-        rst = 1; #20; rst = 0;
-        #200;
-        $display("Simulacao finalizada.");
-        $finish;
+        $dumpfile("waveform.vcd"); $dumpvars(0, dut);
+        rst=1; @(posedge clk); @(posedge clk); rst=0;
+        #2000;
+        dut.reg_file_unit.print_registers;
+        $display("Simulação finalizada."); $finish;
     end
 endmodule
