4d86d60ec324542f4cee436749d05cecdd62e752
Laila13 <77805862+Laila13@users.noreply.github.com>
2025-08-15 21:06:21 -0300

    pc.v

--- pc.v
+++ pc.v
@@ -1,10 +1,4 @@
-// pc.v
-module pc (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire [31:0] pc_next,
-    output reg  [31:0] pc_current
-);
+/module pc (input wire clk, rst, input wire [31:0] pc_next, output reg [31:0] pc_current);
     always @(posedge clk or posedge rst) begin
         if (rst) pc_current <= 32'b0;
         else     pc_current <= pc_next;
